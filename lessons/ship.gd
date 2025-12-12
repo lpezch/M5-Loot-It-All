478e4963bb6aad443f8a58522ecd9afe1dd4ac3d
lpezch <lelopez-chavarria@s.sfusd.edu>
2025-12-11 20:32:25 -0800

    M5L9 Completed

--- lessons/ship.gd
+++ lessons/ship.gd
@@ -1,14 +1,18 @@
 extends Area2D
 
+
 var max_speed := 1200.0
 var velocity := Vector2(0, 0)
 var steering_factor := 3.0
+
 var health := 10
-var gem_count := 0 
+var gem_count := 0
+
 
 func _ready() -> void:
 	area_entered.connect(_on_area_entered)
 	set_health(health)
+
 
 func _process(delta: float) -> void:
 	var direction := Vector2(0, 0)
@@ -22,17 +26,24 @@ func _process(delta: float) -> void:
 	var steering := desired_velocity - velocity
 	velocity += steering * steering_factor * delta
 	position += velocity * delta
+	
+	var viewport_size := get_viewport_rect().size
+	position.x = wrapf(position.x, 0, viewport_size.x)
+	position.y = wrapf(position.y, 0, viewport_size.y)
 
 	if velocity.length() > 0.0:
 		get_node("Sprite2D").rotation = velocity.angle()
+
+
+func set_gem_count(new_gem_count: int) -> void:
+	gem_count = new_gem_count
+	get_node("UI/GemCount").text = "x" + str(gem_count)
+
 
 func set_health(new_health: int) -> void:
 	health = new_health
 	get_node("UI/HealthBar").value = health
 
-func set_gem_count(new_gem_count: int) -> void:
-	gem_count = new_gem_count
-	get_node("UI/GemCount").text = "x" + str(gem_count)
 
 func _on_area_entered(area_that_entered: Area2D) -> void:
 	if area_that_entered.is_in_group("gem"):
